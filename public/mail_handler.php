dd69f8cd06a39856923539845a7243a53dae1a51
ashishraj09 <ashishraj09@gmail.com>
2021-02-23 22:11:56 +1300

    Adding changes to test email

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -11,10 +11,17 @@ if(isset($_POST['submit'])){
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
+    $output = mail($to,$subject,$message);
    //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+        ');';
+            $js_code = '<script>' . $js_code . '</script>';
+            echo $js_code;
     }
+
+
+
 ?>
