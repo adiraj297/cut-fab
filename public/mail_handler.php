e42f90c379e82cbcd5f9bc643a803c3c59f0212b
ashishraj09 <ashishraj09@gmail.com>
2021-02-23 20:03:31 +1300

    Adding changes to test email

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = "renegadewebdev@outlook.com.au"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['firstname'];
     $last_name = $_POST['lastname'];
