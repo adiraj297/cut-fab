b813f32656a8c308c6e56b022dabf5078b5f605b
ashishraj09 <ashishraj09@gmail.com>
2021-02-24 23:00:07 +1300

    Adding email support

--- public/quotes.php
+++ public/quotes.php
@@ -7,6 +7,52 @@
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link href="IMG/favicon.png" rel="icon" type="image/png"/>
+    <style>
+    body {font-family: Arial, Helvetica, sans-serif;}
+
+    /* The Modal (background) */
+    .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 1; /* Sit on top */
+      padding-top: 100px; /* Location of the box */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+
+    /* Modal Content */
+    .modal-content {
+      background-color: #fefefe;
+      margin: auto;
+      padding: 20px;
+      border: 1px solid #888;
+      width: 80%;
+    }
+
+    /* The Close Button */
+    .close {
+      color: #aaaaaa;
+      float: right;
+      font-size: 28px;
+      font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+      color: #000;
+      text-decoration: none;
+      cursor: pointer;
+    }
+    </style>
+ <script>
+
+</script>
+
 </head>
 
 <body>
@@ -20,7 +66,7 @@
     <ul class="nav-links">
         <li><a href="index.html">Home</a></li>
         <li><a href="services.html">Services</a></li>
-        <li><a class="active" href="quotes.html">Quotes</a></li>
+        <li><a class="active" href="quotes.php">Quotes</a></li>
         <li><a href="contact.html">Contact</a></li>
 
     </ul>
@@ -37,21 +83,21 @@
     project:</h3>
 
 <div class="container">
-    <form action="/mail_handler.php" method="post">
+    <form action="send.php" method="post">
         <label for="fname">First Name</label>
-        <input id="fname" name="firstname" placeholder="Your name.." type="text">
+        <input id="fname" name="firstname" required placeholder="Your name.." type="text">
 
         <label for="lname">Last Name</label>
-        <input id="lname" name="lastname" placeholder="Your last name.." type="text">
+        <input id="lname" name="lastname" required  placeholder="Your last name.." type="text">
 
         <label for="phno">Phone Number</label>
-        <input id="phno" name="phno" placeholder="Your phone number.." type="text">
+        <input id="phno" name="phno" required placeholder="Your phone number.." type="text">
 
         <label for="email">Email</label>
-        <input id="email" name="email" placeholder="Your email.." type="text">
+        <input id="email" name="email" required placeholder="Your email.." type="text">
 
         <label for="contact">Preferred Method of contact</label>
-        <select id="contact" name="contact">
+        <select id="contact" required name="contact">
             <option value="Phone">Phone</option>
             <option value="Email">Email</option>
 
@@ -59,16 +105,72 @@
         </select>
 
         <label for="subject">Any queries you may have regarding to your project:</label>
-        <textarea id="subject" name="subject" placeholder="Queries.." style="height:200px"></textarea>
+        <textarea id="message" name="message" placeholder="Queries.." style="height:200px"></textarea>
 
         <input type="submit" value="Submit">
     </form>
 </div>
 
 
-<!--<div class="test">-->
-<!--    <b>This is quotes work in progress!!</b>-->
-<!--</div>-->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p id="output">Email sent successfully..</p>
+  </div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function successful(){
+         const urlParams = new URLSearchParams(window.location.search);
+    var param = urlParams.get('success');
+    console.log('param '+param);
+    if(param === null) {
+    }
+    else
+    {
+        if(param == 'true')
+        {
+        modal.style.display = "block";
+        document.getElementById("output").innerHTML = "Quote request sent successfully!";
+        }
+        else if(param == 'false')
+        {
+        modal.style.display = "block";
+        document.getElementById("output").innerHTML = "Quote request Failed! <br> Please call the number availabe on the contact page.";
+        }
+
+    }
+}
+ window.onload = successful();
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+
+         modal.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+
+</div>
 
 
 <script src="JS/app.js"></script>
